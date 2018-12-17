8aa0ef450a59d89d0cd7ca80a435b1ee74d4e596
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2018-12-17 17:40:51 +0000

    Added the basic static pages sans styling and linked them in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :projects
   root to: 'pages#home'
   get "about", to: "pages#about", as: "about"
+  get "contact", to: "pages#contact", as: "contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
